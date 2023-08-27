fadb61f4100596a7128b5858fdefe4990b1a0626
protobella <marianaleucadio@gmail.com>
2023-08-27 17:40:16 +0100

    Implementação dp drone_object_ros no drone piloting
    Signed-off-by: protobella <marianaleucadio@gmail.com>

--- sjtu-drone/src/drone_piloting.cpp
+++ sjtu-drone/src/drone_piloting.cpp
@@ -1,95 +1,12 @@
+#include "drone_object_ros.h"
 #include <iostream>
-#include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Pose.h>
-#include <ros/ros.h>
-#include <std_msgs/Bool.h>
-#include <std_msgs/Empty.h>
-#include <std_srvs/Empty.h>
 
-ros::Publisher pubTakeOff;
-ros::Publisher pubCmd;
-ros::Publisher pubPosCtrl;
-ros::Publisher pubVelMode;
+unsigned int microsecond = 1000000;
 
 ros::Subscriber pubGtPoseSub;
 
 ros::ServiceClient client;
-
-std_msgs::Bool bool_msg;
-geometry_msgs::Twist twist_msg;
-std_msgs::Empty empty;
-std_srvs::Empty srv;
-
-bool isVelMode = false;
-bool isPosctrl = false;
-bool isFlying = false;
-
-unsigned int microsecond = 1000000;
-
-void moveTo(float x, float y, float z) {
-
-  twist_msg.linear.x = x;
-  twist_msg.linear.y = y;
-  twist_msg.linear.z = z;
-  twist_msg.angular.x = 0;
-  twist_msg.angular.y = 0;
-  twist_msg.angular.z = 0;
-
-  pubCmd.publish(twist_msg);
-  usleep(1 * microsecond);
-  ROS_INFO("Moving to...");
-}
-
-void velMode(bool on) {
-  isVelMode = on;
-
-  if (isVelMode == true) {
-    ROS_INFO("Switching velocity mode on...");
-    bool_msg.data = true;
-    pubVelMode.publish(bool_msg);
-  } else {
-    ROS_INFO("Switching velocity mode off...");
-  }
-}
-
-bool takeOff(void) {
-  if (isFlying)
-    return false;
-
-  pubTakeOff.publish(empty);
-  ROS_INFO("Taking Off...");
-  isFlying = true;
-  usleep(10 * microsecond);
-  return true;
-}
-
-bool hover(void) {
-  if (!isFlying)
-    return false;
-
-  twist_msg.linear.x = 0;
-  twist_msg.linear.y = 0;
-  twist_msg.linear.z = 0;
-  twist_msg.angular.x = 0.0;
-  twist_msg.angular.y = 0.0;
-  twist_msg.angular.z = 0.0;
-
-  pubCmd.publish(twist_msg);
-  ROS_INFO("Hovering...");
-  usleep(10 * microsecond);
-  return true;  
-}
-
-void posCtrl(bool on) {
-  isPosctrl = on;
-
-  bool_msg.data = on ? 1 : 0;
-  pubPosCtrl.publish(bool_msg);
-  if (on)
-    ROS_INFO("Switching position control on...");
-  else
-    ROS_INFO("Switching position control off...");
-}
 
 void PositionCallback(const geometry_msgs::PoseConstPtr& msg) {
    ROS_INFO("Position X: [%If] \n\t\t\t\t Position Y: [%If] \n\t\t\t\t Position Z: [%If] \n", msg->position.x,msg->position.y, msg->position.z);
@@ -97,28 +14,29 @@ void PositionCallback(const geometry_msgs::PoseConstPtr& msg) {
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "drone_piloting");
-
+  
   ros::NodeHandle node;
 
-  pubTakeOff = node.advertise<std_msgs::Empty>("/drone/takeoff", 1, true);
-  pubPosCtrl = node.advertise<std_msgs::Bool>("/drone/posctrl", 1024);
-  pubCmd = node.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
-  pubVelMode = node.advertise<std_msgs::Bool>("/drone/vel_mode", 1024);
-
-  velMode(false);
-  takeOff();
-  posCtrl(false);
-  //moveTo(38,-1.85,2);
-  //hover();
-
+  DroneObjectROS drone(node);
+  
+  drone.velMode(false);
+  
+  drone.takeOff();
+  usleep(1 * microsecond);
+  drone.posCtrl(false);
+  
+  drone.rise(3);
+  usleep(1 * microsecond);
+  drone.hover();
+    
   // generate mesh file
-  client = node.serviceClient<std_srvs::Empty>("/voxblox_node/generate_mesh");
-  if (client.call(srv)) {
-    ROS_INFO("Success to call service voxblox_node/generate_mesh!");
-  } else {
-    ROS_ERROR("Failed to call service voxblox_node/generate_mesh");
-    return 1;
-  }
+  // client = node.serviceClient<std_srvs::Empty>("/voxblox_node/generate_mesh");
+  // if (client.call(srv)) {
+  //   ROS_INFO("Success to call service voxblox_node/generate_mesh!");
+  // } else {
+  //   ROS_ERROR("Failed to call service voxblox_node/generate_mesh");
+  //   return 1;
+  // }
 
   // position of drone 
   //pubGtPoseSub = node.subscribe("drone/gt_pose", 1024, PositionCallback);
