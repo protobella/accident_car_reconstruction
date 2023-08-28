420b7aece6ab57152b7b0b3aa556a88e18ca197e
protobella <marianaleucadio@gmail.com>
2023-08-28 19:41:47 +0100

    [Drone_Stages]: Implementation of stages
    * Implementation of rising stage
    * Prototyping opencv manipulation of the areal view
    
    Signed-off-by: protobella <marianaleucadio@gmail.com>

--- sjtu-drone/src/drone_piloting.cpp
+++ sjtu-drone/src/drone_piloting.cpp
@@ -1,34 +1,201 @@
 #include "drone_object_ros.h"
+
 #include <iostream>
+#include <vector>
 #include <geometry_msgs/Pose.h>
 
-unsigned int microsecond = 1000000;
+#include <image_transport/image_transport.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 
-ros::Subscriber pubGtPoseSub;
+#include <cmath>
 
+// Subscriber
+ros::Subscriber robotpos;
+ros::Subscriber down_camera;
+
+// Service
 ros::ServiceClient client;
 
-void PositionCallback(const geometry_msgs::PoseConstPtr& msg) {
-   ROS_INFO("Position X: [%If] \n\t\t\t\t Position Y: [%If] \n\t\t\t\t Position Z: [%If] \n", msg->position.x,msg->position.y, msg->position.z);
+// waypoint variables
+geometry_msgs::Point drone_position;
+std::vector<cv::Point2f> keypoints_as_waypoints;
+std::vector<cv::Point3d> waypoints;
+std::vector<cv::KeyPoint> keypoints;
+std::vector<int> idx;
+int i;
+double rise, pitch, roll;
+
+// common variables
+int flag = 0;
+bool activate_camera = false;
+
+DroneObjectROS* drone;
+
+void setDrone(DroneObjectROS& drone_) {
+  drone = &drone_;
+  ROS_INFO("SetDrone...");
+}
+
+void stageone() {
+  drone->velMode(false);
+  drone->takeOff();
+  drone->posCtrl(false);
+  drone->rise(1);
+
+  flag = 1;
+}
+double radians(double degree) {
+  double pi = 3.14159265359;
+  return (degree * (pi / 180));
+}
+
+void keypoint_to_coordinate_translate() {
+  double height = 480, width= 640;
+  double fov = 45;
+
+  double altitude = drone_position.z;
+  double fov_radians = radians(fov);
+
+  double scale_x, scale_y;
+
+  scale_x = (2*altitude*tan(fov_radians/2))/width;
+  scale_x = (2*altitude*tan(fov_radians/2))/height;
+  
+
+  for (const cv::KeyPoint& keypoint : keypoints) {
+    double pixel_x = keypoint.pt.x;
+    double pixel_y = keypoint.pt.y;
+
+    double real_x = pixel_x * scale_x;
+    double real_y = pixel_y * scale_y;
+
+    cv::Point3d waypoint(real_x+drone_position.x, real_y+drone_position.y, 3.0);
+    waypoints.push_back(waypoint);
+  }
+}
+
+void waypoint_move(double x, double y, double z, geometry_msgs::Point dposition) {
+  ROS_INFO("Moving to (%f,%f,%f)", x, y, z);
+
+  if (dposition.z >= z) {
+    rise = -0.4;
+    ROS_INFO("Drone going down");
+  } else if (dposition.z < z) {
+    rise = 0.4;
+    ROS_INFO("Drone going up");
+  }
+  if (dposition.x >= x) {
+    pitch = -0.4;
+    ROS_INFO("Drone going back");
+  } else if (dposition.x < x) {
+    pitch = 0.4;
+    ROS_INFO("Drone going front");
+  }
+  if (dposition.y >= y) {
+    roll = -0.4;
+    ROS_INFO("Drone going right");
+  } else if (dposition.y < y) {
+    roll = 0.4;
+    ROS_INFO("Drone going left");
+  }
+
+  drone->moveTo(pitch, roll, rise);
+
+  if (((x-0.5 < dposition.x) && (dposition.x < x+0.5)) && ((y-0.5 < dposition.y) && (dposition.y < y+0.5)) && ((z-0.5 < dposition.z) && (dposition.z < z+0.5))) {
+    drone->hover();
+
+    i++;
+  }
+}
+
+void poscallback (const geometry_msgs::Pose::ConstPtr &msg) {
+  drone_position = msg->position;
+  ROS_INFO("x=%f, y=%f, z=%f", drone_position.x, drone_position.y, drone_position.z);
+  if (flag == 0) {
+    stageone();
+  }
+  if ((flag ==1) && (drone_position.z > 10)) {
+    drone->hover();
+    activate_camera = true;
+  }
+  if (flag == 2) {
+    //TODO: function which translates  keypoints_as_waypoints to real world coordinates
+    keypoint_to_coordinate_translate();
+    // start movement to waypoints
+    flag = 3;
+  }
+  if (flag == 3) {
+    if (i < (sizeof(waypoints)/sizeof(waypoints[0]))) {
+      waypoint_move(waypoints[i].x, waypoints[i].y, waypoints[i].z, drone_position);
+    }
+    else {
+      flag = 4;
+    }
+    // for (int i = 0; i < keypoints_as_waypoints.size(); i++) {
+    //   //TODO: get a function that controls drone to move in the direction of the waypoint
+    //   //https://stackoverflow.com/questions/21613246/what-is-the-structure-of-point2f-in-opencv
+    //   drone->moveTo(keypoints_as_waypoints[i].y, keypoints_as_waypoints[i].x, 1);
+    //   while (distance_to_waypoint(i)>1);
+    // }
+  }
+  if (flag == 4) {
+    //die
+  }
+}
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+  if (activate_camera) {
+    try {
+      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+
+      cv::Mat gray_image;
+      cv::cvtColor(cv_ptr->image, gray_image, cv::COLOR_BGR2GRAY);
+
+      cv::GaussianBlur(gray_image, gray_image, cv::Size(5,5), 0);
+
+      cv::Ptr<cv::Feature2D> fdetector = cv::ORB::create();
+
+      cv::Mat descriptors;
+      fdetector->detectAndCompute(gray_image, cv::Mat(), keypoints, descriptors);
+
+      cv::Mat image_with_keypoints;
+      cv::drawKeypoints(gray_image, keypoints, image_with_keypoints);
+      //cv::KeyPoint::convert(keypoints, keypoints_as_waypoints, idx);
+      // cv::imshow("Manipulated Image With Keypoints", image_with_keypoints);
+      // cv::waitKey(30);
+      flag = 2;
+    }
+    catch (cv_bridge::Exception& e) {
+      ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    }
+  }
+  activate_camera = false;
 }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "drone_piloting");
-  
+
   ros::NodeHandle node;
 
   DroneObjectROS drone(node);
+
+  setDrone(drone);
   
-  drone.velMode(false);
+  // position of drone 
+  robotpos = node.subscribe<geometry_msgs::Pose>("drone/gt_pose",1,poscallback);
   
-  drone.takeOff();
-  usleep(1 * microsecond);
-  drone.posCtrl(false);
-  
-  drone.rise(3);
-  usleep(1 * microsecond);
-  drone.hover();
-    
+  // image callback
+  // cv::namedWindow("Manipulated Image With Keypoints");
+  image_transport::ImageTransport it(node);
+  image_transport::Subscriber sub = it.subscribe("/drone/down_camera/image_raw", 1, imageCallback);
+  ros::spin();
+  // cv::destroyWindow("Manipulated Image With Keypoints");
+
   // generate mesh file
   // client = node.serviceClient<std_srvs::Empty>("/voxblox_node/generate_mesh");
   // if (client.call(srv)) {
@@ -37,11 +204,4 @@ int main(int argc, char **argv) {
   //   ROS_ERROR("Failed to call service voxblox_node/generate_mesh");
   //   return 1;
   // }
-
-  // position of drone 
-  //pubGtPoseSub = node.subscribe("drone/gt_pose", 1024, PositionCallback);
-
-  ros::spin();
-
-  return 0;
 }
